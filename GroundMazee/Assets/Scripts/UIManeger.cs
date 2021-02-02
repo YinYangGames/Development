ea31abd7f548574a07e60e63b30f58918cf90272
ahmedoksuz <34533185+ahmedoksuz@users.noreply.github.com>
2021-02-02 15:50:10 +0300

    2 şubat 15:49
    Timer ve Complated

---
+++ GroundMazee/Assets/Scripts/UIManeger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class UIManeger : MonoBehaviour
+{
+
+    [SerializeField]
+    public TextMeshProUGUI TimerTMP;
+
+
+    float second = 0;
+    int minute=0;
+
+
+
+
+    void Start()
+    {
+        timerIsStart = true;
+    }
+
+    // Update is called once per frame
+
+
+
+    bool timerIsStart;
+    void Update()
+    {
+        if (timerIsStart)
+        {
+            if (second < 60)
+            {
+                second += Time.deltaTime;
+            }
+            else if (second >= 60)
+            {
+                second = 0;
+                minute++;
+            }
+
+            TimerTMP.GetComponent<TextMeshProUGUI>().text = string.Format("{0} : {1}", minute,Convert.ToInt32(second));
+
+
+        }
+
+    }
+
+
+
+
+
+}
